e33720da71e33ea0111132e55273ea95fbbc60aa
Nguyễn Tấn Đạt <tandat2005nt@gmail.com>
2026-03-11 14:40:24 +0700

    Bài thi thử GK_II

---
+++ ntuCMS/src/test/java/thiGK/ntu65130417/nguyentandat_fitCMS/NtuCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65130417.nguyentandat_fitCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtuCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
